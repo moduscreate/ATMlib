ff0191a62a2f953918217738f274229bf6a2e3ae
Delio Brignoli <brignoli.delio@gmail.com>
2017-11-02 16:28:07 +0100

    Move last extern declaration from ATMlib.cpp to atm_synth.h
    Also move osc array instance to isr.c

--- src/isr.c
+++ src/isr.c
@@ -7,6 +7,8 @@ bool half __attribute__((used));
 uint16_t __attribute__((used)) cia;
 uint16_t __attribute__((used)) cia_count;
 
+struct osc osc[4];
+
 #define ATMLIB_CONSTRUCT_ISR(TARGET_REGISTER) \
 ISR(TIMER4_OVF_vect, ISR_NAKED) { \
   asm volatile( \
